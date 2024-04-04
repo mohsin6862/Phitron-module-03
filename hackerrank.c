cb5227935de2bab38106b73199dc2b1872d7e62d
mohsin6862 <mohsinctg2206@gmail.com>
2024-04-04 16:31:03 +0600

    hackerrank solultion added

---
+++ hackerrank.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    long long int b;
+    float c;
+    char d;
+
+
+    scanf("%d", &a);
+
+    scanf("%lld", &b);
+
+
+    scanf("%f", &c);
+
+    scanf(" %c", &d); // Note the space before %c to consume any whitespace characters
+
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%.2f\n", c);
+    printf("%c\n", d);
+
+    return 0;
+}
